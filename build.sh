29fcb0ff838b5f726f4d0f0bc3617c1b444eb939
Bahul Neel Upadhyaya <bahul.upadhyaya@contxt.id>
2023-12-08 17:31:37 +0000

    Refactor build scripts and add Jest for testing
    This commit refactors the build.sh and run_tests_all.sh scripts to
    replace the 'compile' command with 'release' for shadow-cljs, using the
    ':esm' and ':npm' targets. Additionally, the package.json file is
    updated to use Jest as the testing command.
    
    By switching to the 'release' command during the build process, it
    ensures that the final artifacts are optimized for deployment. This
    improves the efficiency of the build by eliminating unnecessary steps
    and reducing file size.
    
    Integrating Jest as the testing framework provides a more comprehensive
    and standardized testing experience. It offers features like verbose
    logging and allows tests to be written in the js/index.test.mjs file.
    This improves the maintainability and scalability of the test suite.
    
    These changes enhance the build and testing processes, resulting in more
    efficient deployments and a robust testing framework.
    
    No issue references.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ clojure -T:build clean
 clojure -T:build jar :version '"'$TP_PONDERMATIC_VERSION'"'
 clojure -T:build install :version '"'$TP_PONDERMATIC_VERSION'"'
 rm-rf dist
-shadow-cljs compile :esm :npm
+shadow-cljs release :esm :npm
 
 cat >dist/cjs/package.json <<!EOF
 {
