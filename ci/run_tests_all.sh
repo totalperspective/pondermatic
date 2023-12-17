653f584abc8899df47a79a98c6cc6a627de0a5b0
Bahul Neel Upadhyaya <bahul.upadhyaya@contxt.id>
2023-12-17 16:04:23 +0000

    Update test runner for js

--- ci/run_tests_all.sh
+++ ci/run_tests_all.sh
@@ -4,5 +4,4 @@ base="$(dirname "$0")"
 ./$base/run_tests_jvm.sh
 ./$base/run_tests_node.sh
 ./$base/run_tests_browser.sh
-shadow-cljs release :esm :npm
-npm test
+./$base/run_tests_js.sh
--- ci/run_tests_js.sh
+++ ci/run_tests_js.sh
@@ -2,4 +2,4 @@
 
 echo "Running NodeJS tests"
 npx shadow-cljs release :npm :esm
-env NODE_OPTIONS=--experimental-vm-modules npx jest
+npx --node-options=--experimental-vm-modules jest
