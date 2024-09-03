b1b3f9214e45eb9d5dcb83d356382d8f5b9665d0
Bahul Neel Upadhyaya <bahul.upadhyaya@contxt.id>
2024-09-03 17:21:45 +0100

    update build scripts

--- release.sh
+++ release.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
+if [ -z "$CLOJARS_USERNAME" ]; then
+  echo "CLOJARS_USERNAME is not set"
+  exit 1
+fi
+if [ -z "$CLOJARS_PASSWORD" ]; then
+  echo "CLOJARS_PASSWORD is not set"
+  exit 1
+fi
+CURRENT_VERSION=$(cat VERSION)
 git flow release start $1
-./build.sh || exit $?
+VERSION=$(cat VERSION)
+
+if [ "$CURRENT_VERSION" = "$VERSION" ]; then
+  echo "Version is not changed, aborting release"
+  git checkout develop
+  git flow release delete -f $VERSION
+  exit 1
+fi
+./build.sh $VERSION || exit $?
 ./deploy.sh || exit $?
 git commit -am "Build artefacts"
 git flow release finish
