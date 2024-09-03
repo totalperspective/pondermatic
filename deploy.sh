a23fa429897836a17853ae19c0a623818a85b29d
Bahul Neel Upadhyaya <bahul.upadhyaya@contxt.id>
2024-09-03 19:47:34 +0100

    Rekove source maps

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+if [ -f .env ]; then
+  source .env
+fi
+
+if [ -z "$CLOJARS_USERNAME" ]; then
+  echo "CLOJARS_USERNAME is not set"
+  exit 1
+fi
+if [ -z "$CLOJARS_PASSWORD" ]; then
+  echo "CLOJARS_PASSWORD is not set"
+  exit 1
+fi
+export CLOJARS_USERNAME=$CLOJARS_USERNAME
+export CLOJARS_PASSWORD=$CLOJARS_PASSWORD
+
 TP_PONDERMATIC_VERSION=`cat VERSION`
 
 clojure -T:build deploy :version '"'$TP_PONDERMATIC_VERSION'"' :sign-releases true || exit $?
