e59ab58767fe6010e28e979d99708c0a05884e3c
Bahul Neel Upadhyaya <bahul.upadhyaya@contxt.id>
2024-10-09 07:40:31 +0100

    Ensure formatter only registers once

--- js/browser.mjs
+++ js/browser.mjs
@@ -1,11 +1,14 @@
 import pFn from '../dist/browser/index.js'
 
 const p = typeof pFn === 'function' ? pFn() : pFn
+p.devtoolsFormatter.id = '__pondermatic__'
 
 if (globalThis.window) {
   globalThis.window.pondermatic = p
   window.devtoolsFormatters = window.devtoolsFormatters || []
-  window.devtoolsFormatters.push(p.devtoolsFormatter)
+  if (!window.devtoolsFormatters.some(f => f.id === p.devtoolsFormatter.id)) {
+    window.devtoolsFormatters.push(p.devtoolsFormatter)
+  }
 }
 
 export default p
