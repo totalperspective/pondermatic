2600ae090a5f5574de669e6120893dcc10c27ee9
Bahul Neel Upadhyaya <bahul.upadhyaya@contxt.id>
2024-10-09 07:30:26 +0100

    Add new taps

--- js/browser.mjs
+++ js/browser.mjs
@@ -2,4 +2,10 @@ import pFn from '../dist/browser/index.js'
 
 const p = typeof pFn === 'function' ? pFn() : pFn
 
+if (globalThis.window) {
+  globalThis.window.pondermatic = p
+  window.devtoolsFormatters = window.devtoolsFormatters || []
+  window.devtoolsFormatters.push(p.devtoolsFormatter)
+}
+
 export default p
